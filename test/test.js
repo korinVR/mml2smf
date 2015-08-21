fa7d69064c2200e6a8479d63541f2011b8e7b7d4
Katsuomi Kobayashi <katsuomi.kobayashi@gmail.com>
2015-08-22 05:33:30 +0900

    Added hex2array and multitrack test

--- test/test.js
+++ test/test.js
@@ -7,42 +7,59 @@ function createSMF(timebase, ...trackDatas) {
 	
 	let smf = [0x4d, 0x54, 0x68, 0x64];
 	
-	function pushUint16(value) {
+	function write2bytes(value) {
 		smf.push((value >> 8) & 0xff, value & 0xff);
 	}
-	function pushUint32(value) {
+	function write4bytes(value) {
 		smf.push((value >> 24) & 0xff, (value >> 16) & 0xff, (value >> 8) & 0xff, value & 0xff);
 	}
 	
-	pushUint32(6);
-	pushUint16(format);
-	pushUint16(trackDatas.length);
-	pushUint16(timebase);
+	write4bytes(6);
+	write2bytes(format);
+	write2bytes(trackDatas.length);
+	write2bytes(timebase);
 	
 	for (let trackData of trackDatas) {
 		smf.push(0x4d, 0x54, 0x72, 0x6b);
-		pushUint32(trackData.length);
+		write4bytes(trackData.length);
 		smf = smf.concat(trackData);
 	}
 	return new Uint8Array(smf);
 }
 
+function hex2array(hex) {
+	let array = [];
+	for (let byte of hex.split(" ")) {
+		array.push(parseInt(byte, 16));
+	}
+	return array;
+}
+
 describe("mml2smf", function () {
 	it("minimum SMF", function () {
 		let mml = "c";
-		let smf = createSMF(480, [0x00, 0x90, 0x3c, 0x64, 0x82, 0x68, 0x80, 0x3c, 0x00]);
+		let smf = createSMF(480, hex2array("00 90 3c 64 82 68 80 3c 00"));
 		assert.deepEqual(mml2smf(mml), smf);
 	});
 	
 	it("tempo", function () {
 		let mml = "t120";
-		let smf = createSMF(480, [0x00, 0xff, 0x51, 0x03, 0x07, 0xa1, 0x20]);
+		let smf = createSMF(480, hex2array("00 ff 51 03 07 a1 20"));
 		assert.deepEqual(mml2smf(mml), smf);
 	});
 	
 	it("control change", function () {
 		let mml = "B10,20";
-		let smf = createSMF(480, [0x00, 0xb0, 0x0a, 0x14]);
+		let smf = createSMF(480, hex2array("00 b0 0a 14"));
+		assert.deepEqual(mml2smf(mml), smf);
+	});
+	
+	it("multitrack", function () {
+		let mml = "c;e;g";
+		let smf = createSMF(480,
+			hex2array("00 90 3c 64 82 68 80 3c 00"),
+			hex2array("00 91 40 64 82 68 81 40 00"),
+			hex2array("00 92 43 64 82 68 82 43 00"));
 		assert.deepEqual(mml2smf(mml), smf);
 	});
 });
